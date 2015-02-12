133d6a41e600ad9c500f3524d068a90bdc2d44e7
Chad Kittel <chad.kittel@gmail.com>
2015-02-11 19:50:59 -0600

    hide more of the Http Response classes

--- BggSharp/Models/HttpResponse/HotItems/Thumbnail.cs
+++ BggSharp/Models/HttpResponse/HotItems/Thumbnail.cs
@@ -2,7 +2,8 @@
 
 namespace BggSharp.Models.HttpResponse.HotItems
 {
-    public class Thumbnail
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated by RestSharp")]
+    internal class Thumbnail
     {
         [DeserializeAs(Name = "value")]
         public string Value { get; set; }
--- BggSharp/Models/HttpResponse/HotItems/YearPublished.cs
+++ BggSharp/Models/HttpResponse/HotItems/YearPublished.cs
@@ -2,7 +2,9 @@
 
 namespace BggSharp.Models.HttpResponse.HotItems
 {
-    public class YearPublished
+    // TODO: Convert to a generic ValueOnly class that we can use for all of these "Value Only" responses
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated by RestSharp")]
+    internal class YearPublished
     {
         [DeserializeAs(Name = "value")]
         public string Value { get; set; }
